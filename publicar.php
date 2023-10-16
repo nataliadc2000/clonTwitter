a73cb73ffa659f33e9296f826404573bb8f92198
Natalia Domínguez <ndcarbajosa@gmail.com>
2023-10-16 20:33:00 +0200

    funcionamiento en los register

--- publicar.php
+++ publicar.php
@@ -1,10 +1,23 @@
 
 <?php
-    if(isset($_POST["submitTweet"])){
-        require_once("./connection/connection.php");
+ require_once "./connection/connection.php";
 
+if(isset($_POST["submitTweet"])) {
+    $id = 0;
 
-        $username = isset($_POST["text"]) ? mysqli_real_escape_string($connect, $_POST["text"]) : false;
-   
+    $user_id = 3;
+    $text = isset($_POST["text"]) ? mysqli_real_escape_string($connect,$_POST["text"]): false;
+
+    $sql = "INSERT INTO publications VALUES ('$id','$user_id','$text',CURDATE())";
+    $guardar = mysqli_query($connect,$sql);
+
+    if($guardar){
+        $_SESSION["completado"] ="Registro completado";
+    } else {
+        $_SESSION["errores"]["general"] = "Fallo en el registro";
     }
+
+    header("Location:./paginaPrincipal.php");
+}
+    
 ?>
